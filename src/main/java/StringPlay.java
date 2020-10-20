74c345d0e7bf45966fe405ddcbb8ae89433ab3f0
Alexandra Hamner <alexandra.m.hamner@gmail.com>
2020-10-20 13:55:01 -0500

    feat: completed the exercise.

--- src/main/java/StringPlay.java
+++ src/main/java/StringPlay.java
@@ -1,12 +1,20 @@
 import org.apache.commons.lang3.StringUtils;
+import java.util.Scanner;
 
 public class StringPlay {
-    public static String capitalizeLetter(String letter){
-        return StringUtils.upperCase(letter);
-        //return letter.toUpperCase();
-    }
 
     public static void main(String[] args) {
-        System.out.println(capitalizeLetter("a"));
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter something:");
+        String input = sc.nextLine();
+        System.out.println("You entered: \"" + input + "\"");
+        if(StringUtils.isNumeric(input)) {
+            System.out.println("\"" + input + "\" is a number");
+        } else {
+            System.out.println("\"" + input + "\" is not a number");
+        }
+
+        System.out.println("Flipped case: " + StringUtils.swapCase(input));
+        System.out.println("Reversed case: " + StringUtils.reverse(input));
     }
 }
